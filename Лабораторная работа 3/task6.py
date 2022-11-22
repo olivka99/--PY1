85184525a6eb9b4c7fae15117684e6c0f720645a
olivka99 <118620801+olivka99@users.noreply.github.com>
2022-11-22 12:54:20 +0300

    Add files via upload

---
+++ Лабораторная работа 3/task6.py
@@ -0,0 +1,16 @@
+list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO Оформить решение
+
+max_number = list_numbers[0]
+max_index = 0
+
+for i, current_number in enumerate(list_numbers):  # перебираем числа
+    max_number = list_numbers[max_index]
+    if current_number >= max_number:  # если текущее число больше того, что встречали ранее
+        max_index = i  # то перезаписываем индекс максимального числа
+        max_number = list_numbers[max_index]  # и перезаписываем число
+
+list_numbers[-1], list_numbers[max_index] = list_numbers[max_index], list_numbers[-1]  # меняем местами максимальное и последнее число
+
+print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
