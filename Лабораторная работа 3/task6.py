fcfe9727f712b23175e5a28d765d09ccc2868300
olivka99 <118620801+olivka99@users.noreply.github.com>
2022-11-26 12:07:43 +0300

    Update task6.py

--- Лабораторная работа 3/task6.py
+++ Лабораторная работа 3/task6.py
@@ -2,14 +2,13 @@ list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38
 
 # TODO Оформить решение
 
-max_number = list_numbers[0]
 max_index = 0
+max_number = list_numbers[0]
 
 for i, current_number in enumerate(list_numbers):  # перебираем числа
-    max_number = list_numbers[max_index]
     if current_number >= max_number:  # если текущее число больше того, что встречали ранее
         max_index = i  # то перезаписываем индекс максимального числа
-        max_number = list_numbers[max_index]  # и перезаписываем число
+        max_number = current_number  # и перезаписываем число
 
 list_numbers[-1], list_numbers[max_index] = list_numbers[max_index], list_numbers[-1]  # меняем местами максимальное и последнее число
 
