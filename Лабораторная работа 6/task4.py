5d0cb93d806f66fd20f5db3b4e7dcd7907a9fd36
olivka99 <118620801+olivka99@users.noreply.github.com>
2023-01-07 08:42:14 +0300

    Update task4.py

--- Лабораторная работа 6/task4.py
+++ Лабораторная работа 6/task4.py
@@ -3,11 +3,10 @@ import json
 INPUT_FILE = "input_new.csv"
 
 
-def csv_to_list_dict(file_in) -> list[dict]:  # TODO реализовать конвертер из csv в json
+def csv_to_list_dict(file_in, a='\n', b=',') -> list[dict]:  # TODO реализовать конвертер из csv в json
     with open(file_in) as file:
-        lines = file.read().split('\n')
-        print(lines)
-        row = [a.split(',') for a in lines]
+        lines = file.read().split(a)
+        row = [i.split(b) for i in lines]
         list_d = [dict(zip(row[0], row[i])) for i in range(1, len(lines))]
     return list_d
 
